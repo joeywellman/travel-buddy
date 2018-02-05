656a027dbfea5ca8613392ce625de50737154de0
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-02-04 18:17:16 -0600

    set up controllers

---
+++ app/controllers/browse-trips.js
@@ -0,0 +1,5 @@
+'use strict';
+
+angular.module("TravelBuddy").controller("BrowseTripsCtrl", function ($scope) {
+  $scope.title = "This is the Brose All Trips View!";
+});
---
+++ app/controllers/trip-builder.js
@@ -0,0 +1,4 @@
+'use strict';
+angular.module("TravelBuddy").controller("TripBuilderCtrl", function ($scope) {
+  $scope.title = "This is the Edit Trip View!";
+});
