4604de2597cc5d3f7995af932227c49d034f18b0
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-02-06 11:41:36 -0600

    added heroku proxy server

--- app/factories/google-places-factory.js
+++ app/factories/google-places-factory.js
@@ -4,7 +4,7 @@ angular.module("TravelBuddy").factory("GMapsFactory", (GMapsCreds, $http, $q) =>
 
   function placesSearch(searchString) {
     return $q((resolve, reject) => {
-      $http.get(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${searchString}&key=${GMapsCreds.apiKey}`)
+      $http.get(`https://travel-buddy-proxy-server.herokuapp.com/api/maps/api/place/textsearch/json?query=${searchString}&key=${GMapsCreds.apiKey}`)
         .then((places) => {
           resolve(places.data.results);
         });
