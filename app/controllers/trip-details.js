98ff4208b130974550d98592bac40d3042433d58
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-02-08 11:01:28 -0600

    moved map to single trip view

--- app/controllers/trip-details.js
+++ app/controllers/trip-details.js
@@ -1,6 +1,15 @@
 'use strict';
-angular.module("TravelBuddy").controller("TripDetailsCtrl", function ($scope, TripFactory, $routeParams) {
+angular.module("TravelBuddy").controller("TripDetailsCtrl", function ($scope, TripFactory, $routeParams, GMapsCreds, NgMap) {
   $scope.title = "This is the trip details controller";
+
+  $scope.center = "41,-87";
+  $scope.googleMapsUrl = `https://maps.googleapis.com/maps/api/js?key=${GMapsCreds.apiKey}`;
+  NgMap.getMap().then(function (map) {
+    console.log(map.getCenter());
+    console.log('markers', map.markers);
+    console.log('shapes', map.shapes);
+  });
+
 
   // TripFactory.getTripDetails($routeParams.tripId)
   // should get back one trip object
