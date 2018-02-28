8e47b98a577faa71b5e8ed1f2d99b4bb1b2c69c2
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-02-28 11:35:13 -0600

    descriptions show up when you edit a trip

--- app/controllers/edit-trip.js
+++ app/controllers/edit-trip.js
@@ -1,15 +1,26 @@
 'use strict';
 angular.module("TravelBuddy").controller("EditTripCtrl", function ($scope, $controller, $routeParams, TripFactory, TripBuilderFactory, GMapsFactory, GMapsCreds, $location) {
-  // inherits places search and functions that let users toggle through search result reviews
-  // inherits add to trip, remove from trip, and rearrange order
-  $controller("TripBuilderCtrl", { $scope: $scope });
   
   $scope.title = "Edit Your Trip";
   const searchResults = [];
   let reviewsLength = null;
+  let userPlaces = null;
   $scope.isCollapsed = false;
- 
 
+
+  // inherits places search and functions that let users toggle through search result reviews
+  // inherits add to trip, remove from trip, and rearrange order
+  $controller("TripBuilderCtrl", { $scope: $scope });
+
+  // adds creator's descriptions to trip locations
+  const addDescriptions = (googlePlaces) => {
+    let placesWithDescriptions = googlePlaces.map((place, index) => {
+      place.description = userPlaces[index].description;
+      return place;
+    });
+    return placesWithDescriptions;
+  };
+  
   // destructures place data from firebase and adds property of place_id
   const formatPlaceData = fbPlaceData => {
     let formattedData = fbPlaceData.map(place => {
@@ -23,12 +34,13 @@ angular.module("TravelBuddy").controller("EditTripCtrl", function ($scope, $cont
   // fetches place info for the trip you're editing
   TripFactory.getFirebasePlaces(TripBuilderFactory.trip.locations)
     .then(fbPlaceData => { // gets place details from firebase
-      let formattedData = formatPlaceData(fbPlaceData);
-      return GMapsFactory.getGooglePlaces(formattedData);
+      userPlaces= formatPlaceData(fbPlaceData);
+      return GMapsFactory.getGooglePlaces(userPlaces);
     })
     .then(placeDetails => { // gets place details from google places
       $scope.tripLoaded = true;
-      $scope.tripLocations = GMapsFactory.formatPlaces(placeDetails);
+      let googlePlaces = GMapsFactory.formatPlaces(placeDetails);
+      $scope.tripLocations = addDescriptions(googlePlaces);
     });
 
 
