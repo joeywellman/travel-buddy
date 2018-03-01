dde0aceb1d2929997dc9a8e7c6d84a9ed1913b57
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-03-01 06:09:49 -0600

    fixed bug with editing trip and tags fritzing out

--- app/controllers/edit-init.js
+++ app/controllers/edit-init.js
@@ -4,10 +4,28 @@ angular.module("TravelBuddy").controller("EditInitCtrl", function ($scope, $rout
   $scope.title = "Let's make some changes.";
   $scope.route = `#!/edit/search/${$routeParams.tripId}`; // where the onward button will take you
 
+  // accoutns for trips that were created before ng-tags-input was added
+  // converts array of strings into array of objects for ng-tags to print
+  const convertTags = (trip) => {
+    if (trip.tags.length > 0 && trip.tags[0] !== null && typeof trip.tags[0] === 'string'){
+      console.log("this is a trip with string tags!", trip);
+      let formattedTags = trip.tags.map(tag => {
+        console.log("this is a single tag before you format it", tag);
+        tag = {text: tag};
+        console.log("and now it should be an object", tag);
+        return tag;
+      });
+      trip.tags = formattedTags;
+      console.log("trip.tags, should be objects", trip.tags);
+    }
+    return trip;
+  };
+
   // get trip info from firebase and set it to global TripBuilderFactory obj so it can be accessed in this controller and in TripBuilderCtrl
   TripFactory.getTripDetails($routeParams.tripId)
     .then(tripDetails => {
-      tripDetails.tags = tripDetails.tags.join(', ');
+      tripDetails = convertTags(tripDetails);
+      console.log('tripdetails', tripDetails);
       TripBuilderFactory.trip = tripDetails;
       $scope.trip = TripBuilderFactory;
     });
